3da1728b4aa04d07db81f749cd98e197611a6a34
gadamc <adam.cox@kit.edu>
2013-02-22 13:09:28 -0800

    and fixed...

--- views/channel/map.js
+++ views/channel/map.js
@@ -2,7 +2,7 @@ function (doc) {
 
   if(doc.type == 'bbolo_position_map' && doc.bolometer && isArray(doc.channels) && 
     doc.date_valid &&  doc.date_valid.year &&
-    doc.date_valid.month && doc.date_valid.day && !doc.date_valid.hour){
+    doc.date_valid.month && doc.date_valid.day && doc.date_valid.hour === undefined){
     for (var chan in doc.channels) {
        emit( [doc['channels'][chan] + " " + doc.bolometer , doc.date_valid.year, doc.date_valid.month, doc.date_valid.day, 0, 0], 1);        
     }
--- views/date_valid/map.js
+++ views/date_valid/map.js
@@ -1,12 +1,16 @@
 function(doc) {
-    if(doc.type == "bbolo_position_map" && doc.date_valid && doc.date_valid.year &&
-      doc.date_valid.month && doc.date_valid.day && !doc.date_valid.hour){
-	   emit([doc.date_valid.year, doc.date_valid.month, doc.date_valid.day, 0, 0], 1);
+    if(doc.type == "bbolo_position_map" && doc.bolometer && isArray(doc.channels) && doc.date_valid && doc.date_valid.year &&
+      doc.date_valid.month && doc.date_valid.day && doc.date_valid.hour === undefined){
+      for (var chan in doc.channels) {
+	     emit([doc.date_valid.year, doc.date_valid.month, doc.date_valid.day, 0, 0, doc['channels'][chan] + " " + doc.bolometer ], 1);
+      }
     }
 
-    if(doc.type == "bbolo_position_map" && doc.date_valid && doc.date_valid.year && 
+    if(doc.type == "bbolo_position_map" && doc.bolometer && isArray(doc.channels) && doc.date_valid && doc.date_valid.year && 
       doc.date_valid.month && doc.date_valid.day && doc.date_valid.hour && doc.date_valid.minute){
-     emit([doc.date_valid.year, doc.date_valid.month, doc.date_valid.day, doc.date_valid.hour, doc.date_valid.minute], 1);
+      for (var chan in doc.channels) {
+        emit([doc.date_valid.year, doc.date_valid.month, doc.date_valid.day, doc.date_valid.hour, doc.date_valid.minute, doc['channels'][chan] + " " + doc.bolometer ], 1);
+      }
     }
 
 }
